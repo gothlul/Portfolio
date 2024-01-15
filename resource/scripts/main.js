7c32aace6951484fe8919ee1aa266ea0a4f4307e
gothlul <lrasoppi11@gmail.com>
2024-01-15 20:16:19 -0300

    #

--- resource/scripts/main.js
+++ resource/scripts/main.js
@@ -1,10 +1,21 @@
 const navbar = document.querySelector('header');
 const listaProjetos = document.getElementById('lista-projetos');
 const btnProjeto = document.getElementById('btn-proj');
+const body = document.querySelector('body');
 let i = 0;
 
 var projetos = {
-    titulo:['teste1', 'teste2', 'teste3', 'teste4', 'teste5', 'teste6', 'teste7'], descricao:[], capa:[], tecs:['teste1', 'teste2', 'teste3', 'teste4', 'teste5', 'teste6', 'teste7']
+    titulo:['teste1', 'teste2', 'teste3', 'teste4', 'teste5', 'teste6', 'teste7'], 
+    descricao:[], 
+    capa:[], 
+    tecs:[
+        ['../resource/img/icons/icon-css.svg', '../resource/img/icons/icon-html.svg'], 
+        ['../resource/img/icons/icon-css.svg', '../resource/img/icons/icon-html.svg'], 
+        ['../resource/img/icons/icon-css.svg', '../resource/img/icons/icon-html.svg'], 
+        ['../resource/img/icons/icon-css.svg', '../resource/img/icons/icon-html.svg'], 
+        ['../resource/img/icons/icon-css.svg', '../resource/img/icons/icon-html.svg'], 
+        ['../resource/img/icons/icon-css.svg', '../resource/img/icons/icon-html.svg'], 
+        ['../resource/img/icons/icon-css.svg', '../resource/img/icons/icon-html.svg']]
 }
 
 function navScroll(){
@@ -14,12 +25,16 @@ function navScroll(){
 window.addEventListener('scroll', navScroll);
 
 for(i; i < 3; i++){
-    listaProjetos.innerHTML += `<li class="card"><div class="capa"><img src="${projetos.capa[i]}"/></div><div><h3>${projetos.titulo[i]}</h3><p>${projetos.descricao[i]}</p><ul>${contaTec(i)}</ul><div class="view"><p>Visualizar</p></div></div></li>`;
+    listaProjetos.innerHTML += `<li class="card"><div class="capa"><img src="${projetos.capa[i]}"/></div><div><h3>${projetos.titulo[i]}</h3><p>${projetos.descricao[i]}</p><ul id="item-${i}"></ul><div class="view"><p>Visualizar</p></div></div></li>`;
+
+    contaTec(i);
 }
 
 function loading(){
     for(i > 2; i < projetos.titulo.length; i++){
-        listaProjetos.innerHTML += `<li class="card"><div class="capa"><img src="${projetos.capa[i]}"/></div><div><h3>${projetos.titulo[i]}</h3><p>${projetos.descricao[i]}</p><ul>${contaTec(i)}</ul><div class="view"><p>Visualizar</p></div></div></li>`;
+        listaProjetos.innerHTML += `<li class="card"><div class="capa"><img src="${projetos.capa[i]}"/></div><div><h3>${projetos.titulo[i]}</h3><p>${projetos.descricao[i]}</p><ul id="item-${i}"></ul><div class="view"><p>Visualizar</p></div></div></li>`;
+
+        contaTec(i);
     }
 
     btnProjeto.style.display = 'none';
@@ -27,9 +42,18 @@ function loading(){
 
 function contaTec(i){
     let cont = 0;
-    let tecnologia = projetos.tecs[i]
-    while(cont < tecnologia.length){
-        listaProjetos.getElementsByClassName('ul').innerHTML += `<li class="tec-projeto"><img src="${tecnologia[cont]}"/></li>`;
-        cont++;
+    let listaTec = document.getElementById(`item-${i}`);
+    let tecnologia = projetos.tecs[i];
+
+    if(tecnologia.length > 0){
+        while(cont < tecnologia.length){
+            listaTec.innerHTML += `<li class="tec-projeto"><img src="${tecnologia[cont]}"/></li>`;
+            cont++;
+        }
     }
+}
+
+function darkmode(){
+    body.classList.toggle('drk');
+
 }
